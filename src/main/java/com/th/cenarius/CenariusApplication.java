8ba11e58ae7838817b326cb1f9359080c382008f
Aaron <aaron.tan@henkel.com>
2022-02-15 14:08:33 +0800

    Initial commit

---
+++ src/main/java/com/th/cenarius/CenariusApplication.java
@@ -0,0 +1,13 @@
+package com.th.cenarius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenariusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenariusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/th/cenarius/CenariusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th.cenarius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenariusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
